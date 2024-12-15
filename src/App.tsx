7c796349c30cc326cc060365cea7af25102721ee
Avijit Sarkar <avijeett007@gmail.com>
2024-12-15 23:02:29 +0000

    initial plumber google api bot

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { useRef, useState } from "react";
+import "./App.scss";
+import { LiveAPIProvider } from "./contexts/LiveAPIContext";
+import { PlumbingAnalyzer } from "./components/plumbing-analyzer/PlumbingAnalyzer";
+import ControlTray from "./components/control-tray/ControlTray";
+import cn from "classnames";
+
+const API_KEY = process.env.REACT_APP_GEMINI_API_KEY as string;
+if (typeof API_KEY !== "string") {
+  throw new Error("set REACT_APP_GEMINI_API_KEY in .env");
+}
+
+const host = "generativelanguage.googleapis.com";
+const uri = `wss://${host}/ws/google.ai.generativelanguage.v1alpha.GenerativeService.BidiGenerateContent`;
+
+function App() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [videoStream, setVideoStream] = useState<MediaStream | null>(null);
+
+  return (
+    <div className="App">
+      <LiveAPIProvider url={uri} apiKey={API_KEY}>
+        <div className="streaming-console">
+          <main>
+            <div className="main-app-area">
+              <PlumbingAnalyzer />
+              <video
+                className={cn("stream", {
+                  hidden: !videoRef.current || !videoStream,
+                })}
+                ref={videoRef}
+                autoPlay
+                playsInline
+              />
+            </div>
+
+            <ControlTray
+              videoRef={videoRef}
+              supportsVideo={true}
+              onVideoStreamChange={setVideoStream}
+            />
+          </main>
+        </div>
+      </LiveAPIProvider>
+    </div>
+  );
+}
+
+export default App;
